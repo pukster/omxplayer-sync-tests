7e40dcde66f755d48cacf17999d69e4867fac5c5
pukster <peter.peyman.puk@gmail.com>
2013-11-04 22:46:33 -0800

    Create omxplayer-sync-tests.sh
    The scripts which get called on the individual raspi clients.

---
+++ omxplayer-sync-tests.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# A series of commands to indipendantly synchronize the multiple raspis before, hopefully, launching them simultaneously. #
+# It is very important the this script launches omxplayer with no other loops or commands running in the background #
+# as that would prohibitively slow down the rendering of the video file. #
