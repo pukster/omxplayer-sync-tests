011f14262d16e5b0857f7e02b5eb4640dd1fcef3
pukster <peter.peyman.puk@gmail.com>
2013-11-05 12:25:42 -0800

    Update omxplayer-sync-tests.sh
    Finalized script with commenting to make it easy for all users to modify. This program essentially waits, with frequency 0.1s until the desired launch time is encountered. Not only did this prove to be an inaccurate way to carry this out, but it turns out that even if two raspis call omxplayer at the same time, there is still no guarantee that the movies will launch at the same time.

--- omxplayer-sync-tests.sh
+++ omxplayer-sync-tests.sh
@@ -1,5 +1,55 @@
 #!/bin/bash
 
-# A series of commands to indipendantly synchronize the multiple raspis before, hopefully, launching them simultaneously. #
-# It is very important the this script launches omxplayer with no other loops or commands running in the background #
-# as that would prohibitively slow down the rendering of the video file. #
+# Raspberry Pi is, for the lack of a better term, a pice of shit. If   #
+# You have an echo statement running in the background, omxplayer      #
+# whill struggle with HD video. Therefore, synchronizing video will    #
+# require starting at the same time, NOT having a program monitor its  #
+# progress. For this reason, this simple script will sleep at 0.1ms    #
+# intervals and check to see whether it should start or not.           #
+if [ $# -lt 1 ]
+then
+   echo USAGE: ./omxplayer-sync-hack time [ omxplayer-command, ...]
+   exit -1
+fi
+
+HOST=$(hostname)
+curTime=$(($(date +%s%N)/1000000))
+desiredTime=$1 # time to launch all raspis in synchrony
+omxparams=''
+i=0
+
+# Do not pass all params in "$@" as the first parameter is the desired #
+# time.                                                                #
+for param in "$@"
+do
+   if [ $i != 0 ]
+   then
+      omxparams="$omxparams $param"
+   fi
+   i=$(($i+1))
+done
+
+# The leap of faith here is that `sleep 0.1` will work in some         #
+# meaningful way.                                                      #
+while [ $curTime -lt $desiredTime ]
+do
+   curTime=$(($(date +%s%N)/1000000))
+   sleep 0.1
+done
+
+echo desired time: $desiredTime
+echo actual time: $curTime
+echo difference: $(($curTime-$desiredTime))
+
+# Most of the time this script called from one of the raspis, however, #
+# if called from main or the raspiserver, make sure not to call        #
+# omxplayer as it is non existent                                      #
+#                                                                      #
+# NOTE change these machine names to fit your own set up               #
+if [ $HOST != main ] && [ $HOST != raspiserver ]
+then
+   omxplayer $omxparams
+fi
+
+# Return the time difference to omxplayer-sync-test.py                 #
+exit $(($curTime-$desiredTime))
